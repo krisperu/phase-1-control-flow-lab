2ad8891b6ea5060cc9a52226c98417c4b595e731
Kristina Peru <krissy.germanov@gmail.com>
2022-01-31 23:19:57 -0700

    commit tree 55f32fa2302c2aa4751a2a9c3aeff684d7bfb224

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(number){
   // Write your code here!
+  if (number <= 400) {
+    return scuberGreetingForFeet = 'This one is on me!';
+  } else if (number >= 2000 && number <=2500) {
+    return scuberGreetingForFeet = 'I will gladly take your thirty bucks.';
+  } else if (number > 2500) {
+    return scuberGreetingForFeet = 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === 'NYC'){
+    return 'Ok, sounds good.';
+  } else if ( city !== 'NYC') {
+    return 'No go.';
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    case 'thanks for everything':
+      return 'Bye.';
+  }
 }
